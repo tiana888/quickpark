9fa7adc630365f5bc8c5eec6e10167ce4cf363bd
b890615 <b890615@gmail.com>
2023-12-05 21:43:11 +0800

    update spaceZoomPage route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-
 import ClientPage from "./pages/ClientPage";
 import GuardPage from "./pages/GuardPage";
 import HistoryPage from "./pages/HistoryPage";
@@ -15,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/clientpage" element={<ClientPage />} />
-        <Route path="/clientpage/spacezoompage" element={<SpaceZoomPage />} />
+        <Route path="/clientpage/spacezoompage/:floor/:section" element={<SpaceZoomPage />} />
         <Route path="/guardpage" element={<GuardPage />} />
         <Route path="/guardpage/historypage" element={<HistoryPage />} />
         <Route path="/testpage" element={<TestPage />} />
