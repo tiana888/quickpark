77cd26e8aa06b3b2925c7149c3e9d148a2cea922
b890615 <b890615@gmail.com>
2023-12-11 20:49:50 +0800

    update spacezoomUI

--- frontend/src/pages/SpaceZoomPage.tsx
+++ frontend/src/pages/SpaceZoomPage.tsx
@@ -32,50 +32,50 @@ export default function SpaceZoomPage() {
       {/* <Button onClick={}>INIT</Button> */}
 
       <div className="ml-10 ">
-        <p className="text-3xl"> Floor: {floor}  Area: {section} </p>
-        <p className="text-3xl"> 剩餘車位數: {remainingSpaces.length}</p>
+        <p className="text-3xl"> 樓層: {floor}  區域: {section} </p>
+        <p className="text-3xl m-5"> 剩餘車位數: {remainingSpaces.length}</p>
       </div>
-      <div className="border-2 border-white rounded-lg w-auto h-screen m-8" style={{ display: "flex", justifyContent: "space-between", flexDirection: "row" }}>
+      <div className=" border-white rounded-2xl w-auto h-screen m-8" style={{ display: "flex", justifyContent: "space-between", flexDirection: "row",borderWidth: "12px" }}>
  {/* 1-10 */}
-      <div style={{ flex: 1,display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
-        {spaces.map((space) => (
-        space.number <= 10 && (
-          <Square
-            key= {space.number}
-            floor= {space.floor}
-            section= {space.section}
-            number= {space.number}
-            priority= {space.priority}  
-            occupied= {space.occupied}
-            license= {space.license}
-            arrivalTime= {space.arrivalTime}
-            departureTime={space.departureTime}
-            history={space.history}
-          />
-        )
-    ))}
-  </div>
+        <div style={{ flex: 1,display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+          {spaces.map((space) => (
+          space.number <= 10 && (
+            <Square
+              key= {space.number}
+              floor= {space.floor}
+              section= {space.section}
+              number= {space.number}
+              priority= {space.priority}  
+              occupied= {space.occupied}
+              license= {space.license}
+              arrivalTime= {space.arrivalTime}
+              departureTime={space.departureTime}
+              history={space.history}
+            />
+          )
+      ))}
+        </div>
 
-  {/* 11-20 */}
-  <div style={{ flex: 1,display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
-    {spaces.map((space) => (
-      space.number > 10 && space.number <= 20 && (
-        <Square
-          key= {space.number}
-          floor= {space.floor}
-          section= {space.section}
-          number= {space.number}
-          priority= {space.priority}  
-          occupied= {space.occupied}
-          license= {space.license}
-          arrivalTime= {space.arrivalTime}
-          departureTime={space.departureTime}
-          history={space.history}
-        />
-      )
-    ))}
-  </div>
-</div>
+    {/* 11-20 */}
+        <div style={{ flex: 1,display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
+          {spaces.map((space) => (
+            space.number > 10 && space.number <= 20 && (
+              <Square
+                key= {space.number}
+                floor= {space.floor}
+                section= {space.section}
+                number= {space.number}
+                priority= {space.priority}  
+                occupied= {space.occupied}
+                license= {space.license}
+                arrivalTime= {space.arrivalTime}
+                departureTime={space.departureTime}
+                history={space.history}
+              />
+            )
+          ))}
+        </div>
+      </div>
 
     </>
   );
