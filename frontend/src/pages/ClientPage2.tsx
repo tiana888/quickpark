7929a1fd91e5ad4a8ad84d970386753528b84877
b09505051 <b09505051@ntu.edu.tw>
2023-12-14 17:11:27 +0800

    1214

--- frontend/src/pages/ClientPage2.tsx
+++ frontend/src/pages/ClientPage2.tsx
@@ -1,31 +1,131 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
+import React, { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import HeaderBar from "@/components/HeaderBar";
 import NewInput from "@/components/NewInput";
+import { getSpaces } from "@/utils/client";
 
 export default function ClientPage3() {
   // 创建一个数组，包含六个数字（可以是任意数字，这里用索引 i 作为 key）
   const gridItems = Array.from({ length: 6 }, (_, i) => i);
+  const [countsB2A, setCountsB2A] = useState(0);
+  const [countsB2B, setCountsB2B] = useState(0);
+  const [countsB2C, setCountsB2C] = useState(0);
+  const [countsB2D, setCountsB2D] = useState(0);
+  const [countsB2E, setCountsB2E] = useState(0);
+  const [countsB2F, setCountsB2F] = useState(0);
 
   // 可改成其他顏色變換，此為暫時
-  const getColor = (index) => {
-    if (index === 4 || index === 5) {
-      return '#EA8484';
+  const Get_Space = async () => {
+    try {
+      const result = await getSpaces();
+      const allSpace = (await result).data;
+      const B2 = allSpace.slice(allSpace.length / 3, allSpace.length / 3*2);
+
+      const B2A = B2.slice(0, B2.length / 6);
+      const B2B = B2.slice(B2.length / 6, B2.length / 6*2);
+      const B2C = B2.slice(B2.length / 6*2, B2.length /6*3);
+      const B2D = B2.slice(B2.length /6*3, B2.length / 6*4);
+      const B2E = B2.slice(B2.length /6*4, B2.length / 6*5);
+      const B2F = B2.slice(B2.length/6*5, B2.length / 6*6);
+      let countA = 0;
+      let countB = 0;
+      let countC = 0;
+      let countD = 0;
+      let countE = 0;
+      let countF = 0;
+
+      B2A.forEach((space) => {
+        if (space.occupied === true) {
+          countA = countA + 1;
+        }
+      });
+      setCountsB2A(countA);
+
+      B2B.forEach((space) => {
+        if (space.occupied === true) {
+          countB = countB + 1;
+        }
+      });
+      setCountsB2B(countB);
+
+      B2C.forEach((space) => {
+        if (space.occupied === true) {
+          countC = countC + 1;
+        }
+      });
+      setCountsB2C(countC);
+
+      B2D.forEach((space) => {
+        if (space.occupied === true) {
+          countD = countD + 1;
+        }
+      });
+      setCountsB2D(countD);
+
+      B2E.forEach((space) => {
+        if (space.occupied === true) {
+          countE = countE + 1;
+        }
+      });
+      setCountsB2E(countE);
+
+      B2F.forEach((space) => {
+        if (space.occupied === true) {
+          countF = countF + 1;
+        }
+      });
+      setCountsB2F(countF);
+      // Repeat the process for other areas (B1B, B1C, B1D, B1E, B1F)
+
+
+    } catch (error) {
+      console.error('Error fetching space data:', error);
     }
-    return index % 2 === 0 ? '#E8D782' : '#8CDCB6';
   };
 
-  const handleViewClick = () => {
-    // Handle click on the view area
-    // You can add logic to show more information or navigate to another page
-    console.log('View area clicked!');
+  useEffect(() => {
+    Get_Space();
+  }, []); // Empty dependency array ensures it only runs once on mount
+
+  const getColor = (counts) => {
+    if (counts >=10 && counts <= 15) {
+      return '#E8D782'; // Yellow when counts are greater than 0
+    }
+    if (counts >= 0 && counts < 10){
+    return '#8CDCB6'; // Green when counts are 0
+    }
+    if (counts > 15){
+      return '#EA8484'; // Green when counts are 0
+      }
   };
+  const getCountForIndex = (index) => {
+    switch (index) {
+      case 0:
+        return countsB2A;
+      case 1:
+        return countsB2B;
+      case 2:
+        return countsB2C;
+      case 3:
+        return countsB2D;
+      case 4:
+        return countsB2E;
+      case 5:
+        return countsB2F;
+      default:
+        return 0;
+    }
+  };
+
 
   return (
     <>
+     <button onClick={Get_Space} className="border-2 border-white">
+        GET測試
+      </button>
       <HeaderBar />
       <div>ClientPage2</div>
-      <div className="mx-auto border rounded-lg w-2/3" onClick={handleViewClick} style={{ position: 'relative', cursor: 'pointer' }}>
+      <div className="mx-auto border rounded-lg w-2/3" style={{ position: 'relative', cursor: 'pointer' }}>
         <NewInput
           placeholder="請輸入密碼"
         />
@@ -54,9 +154,11 @@ export default function ClientPage3() {
       {/* Grid items */}
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '0px', marginTop: '100px' }}>
         {gridItems.map((item, index) => (
-          <div key={item} style={{ position: 'relative', width: '100%', height: '0', paddingBottom: '100%', border: '15px dashed white', textAlign: 'center', lineHeight: '50px', backgroundColor: getColor(item) }}>
+          <div key={item} style={{ position: 'relative', width: '100%', height: '0', paddingBottom: '100%', border: '15px dashed white', textAlign: 'center', lineHeight: '50px', backgroundColor: getColor(getCountForIndex(index)), }}>
+            
             {index === 0 && (
               <>
+              <Link to= '/clientpage/spacezoompage/B2/A'>
                 <img
                   src="/A.png"
                   alt="A"
@@ -67,10 +169,13 @@ export default function ClientPage3() {
                   alt="Disable"
                   style={{ position: 'absolute', top: '0', right: '0', width: '50px', height: '50px' }}
                 />
+                </Link>
               </>
             )}
+
             {index === 1 && (
               <>
+              <Link to= '/clientpage/spacezoompage/B2/B'>
                 <img
                   src="/B.png"
                   alt="B"
@@ -81,35 +186,48 @@ export default function ClientPage3() {
                   alt="Disable"
                   style={{ position: 'absolute', top: '0', right: '0', width: '50px', height: '50px' }}
                 />
+                </Link>
               </>
             )}
+
             {index === 2 && (
+              <Link to= '/clientpage/spacezoompage/B2/C'>
              <img
              src="/C.png"
              alt="C"
              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '10%', height: '10%' }}
             />
+            </Link>
             )}
+
             {index === 3 && (
+              <Link to= '/clientpage/spacezoompage/B2/D'>
              <img
              src="/D.png"
              alt="D"
              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '10%', height: '10%' }}
             />
+            </Link>
             )}
+
             {index === 4 && (
+              <Link to= '/clientpage/spacezoompage/B2/E'>
              <img
              src="/E.png"
              alt="E"
              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '10%', height: '10%' }}
             />
+            </Link>
             )}
+
             {index === 5 && (
+              <Link to= '/clientpage/spacezoompage/B2/F'>
              <img
              src="/F.png"
              alt="F"
              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '10%', height: '10%' }}
             />
+            </Link>
             )}
           </div>
         ))}
@@ -118,13 +236,13 @@ export default function ClientPage3() {
          {/* Button to switch to ClientPage2 */}
       <Link to="/clientpage">  
         <button className="my-10  bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          切換到ClientPage
+          切換到 B1
         </button>
       </Link>
 
       <Link to="/clientpage/clientpage3">
         <button className="my-10 mx-85 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          切換到 ClientPage3
+          切換到 B3
         </button>
       </Link>
     </>
--- frontend/src/pages/ClientPage3.tsx
+++ frontend/src/pages/ClientPage3.tsx
@@ -1,31 +1,130 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
+import React, { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import HeaderBar from "@/components/HeaderBar";
 import NewInput from "@/components/NewInput";
+import { getSpaces } from "@/utils/client";
 
 export default function ClientPage2() {
   // 创建一个数组，包含六个数字（可以是任意数字，这里用索引 i 作为 key）
   const gridItems = Array.from({ length: 6 }, (_, i) => i);
-
+  const [countsB3A, setCountsB3A] = useState(0);
+  const [countsB3B, setCountsB3B] = useState(0);
+  const [countsB3C, setCountsB3C] = useState(0);
+  const [countsB3D, setCountsB3D] = useState(0);
+  const [countsB3E, setCountsB3E] = useState(0);
+  const [countsB3F, setCountsB3F] = useState(0);
   // 可改成其他顏色變換，此為暫時
-  const getColor = (index) => {
-    if (index === 4 || index === 5) {
-      return '#EA8484';
+  const Get_Space = async () => {
+    try {
+      const result = await getSpaces();
+      const allSpace = (await result).data;
+      const B3 = allSpace.slice(allSpace.length / 3*2, allSpace.length / 3*3);
+
+      const B3A = B3.slice(0, B3.length / 6);
+      const B3B = B3.slice(B3.length / 6, B3.length / 6*2);
+      const B3C = B3.slice(B3.length / 6*2, B3.length /6*3);
+      const B3D = B3.slice(B3.length /6*3, B3.length / 6*4);
+      const B3E = B3.slice(B3.length /6*4, B3.length / 6*5);
+      const B3F = B3.slice(B3.length/6*5, B3.length / 6*6);
+      let countA = 0;
+      let countB = 0;
+      let countC = 0;
+      let countD = 0;
+      let countE = 0;
+      let countF = 0;
+
+      B3A.forEach((space) => {
+        if (space.occupied === true) {
+          countA = countA + 1;
+        }
+      });
+      setCountsB3A(countA);
+
+      B3B.forEach((space) => {
+        if (space.occupied === true) {
+          countB = countB + 1;
+        }
+      });
+      setCountsB3B(countB);
+
+      B3C.forEach((space) => {
+        if (space.occupied === true) {
+          countC = countC + 1;
+        }
+      });
+      setCountsB3C(countC);
+
+      B3D.forEach((space) => {
+        if (space.occupied === true) {
+          countD = countD + 1;
+        }
+      });
+      setCountsB3D(countD);
+
+      B3E.forEach((space) => {
+        if (space.occupied === true) {
+          countE = countE + 1;
+        }
+      });
+      setCountsB3E(countE);
+
+      B3F.forEach((space) => {
+        if (space.occupied === true) {
+          countF = countF + 1;
+        }
+      });
+      setCountsB3F(countF);
+      // Repeat the process for other areas (B1B, B1C, B1D, B1E, B1F)
+
+
+    } catch (error) {
+      console.error('Error fetching space data:', error);
     }
-    return index % 2 === 0 ? '#E8D782' : '#8CDCB6';
   };
 
-  const handleViewClick = () => {
-    // Handle click on the view area
-    // You can add logic to show more information or navigate to another page
-    console.log('View area clicked!');
+  useEffect(() => {
+    Get_Space();
+  }, []); // Empty dependency array ensures it only runs once on mount
+
+  const getColor = (counts) => {
+      if (counts >=10 && counts <= 15) {
+        return '#E8D782'; // Yellow when counts are greater than 0
+      }
+      if (counts >= 0 && counts < 10){
+      return '#8CDCB6'; // Green when counts are 0
+      }
+      if (counts > 15){
+        return '#EA8484'; // Green when counts are 0
+        }
+    };
+  const getCountForIndex = (index) => {
+    switch (index) {
+      case 0:
+        return countsB3A;
+      case 1:
+        return countsB3B;
+      case 2:
+        return countsB3C;
+      case 3:
+        return countsB3D;
+      case 4:
+        return countsB3E;
+      case 5:
+        return countsB3F;
+      default:
+        return 0;
+    }
   };
+
 
   return (
     <>
+    <button onClick={Get_Space} className="border-2 border-white">
+        GET測試
+      </button>
       <HeaderBar />
       <div>ClientPage3</div>
-      <div className="mx-auto border rounded-lg w-2/3" onClick={handleViewClick} style={{ position: 'relative', cursor: 'pointer' }}>
+      <div className="mx-auto border rounded-lg w-2/3" style={{ position: 'relative', cursor: 'pointer' }}>
         <NewInput
           placeholder="請輸入密碼"
         />
@@ -54,9 +153,10 @@ export default function ClientPage2() {
       {/* Grid items */}
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '0px', marginTop: '100px' }}>
         {gridItems.map((item, index) => (
-          <div key={item} style={{ position: 'relative', width: '100%', height: '0', paddingBottom: '100%', border: '15px dashed white', textAlign: 'center', lineHeight: '50px', backgroundColor: getColor(item) }}>
+          <div key={item} style={{ position: 'relative', width: '100%', height: '0', paddingBottom: '100%', border: '15px dashed white', textAlign: 'center', lineHeight: '50px', backgroundColor: getColor(getCountForIndex(index)) }}>
             {index === 0 && (
               <>
+              <Link to= '/clientpage/spacezoompage/B3/A'>
                 <img
                   src="/A.png"
                   alt="A"
@@ -67,10 +167,12 @@ export default function ClientPage2() {
                   alt="Disable"
                   style={{ position: 'absolute', top: '0', right: '0', width: '50px', height: '50px' }}
                 />
+                </Link>
               </>
             )}
             {index === 1 && (
               <>
+              <Link to= '/clientpage/spacezoompage/B3/B'>
                 <img
                   src="/B.png"
                   alt="B"
@@ -81,35 +183,46 @@ export default function ClientPage2() {
                   alt="Disable"
                   style={{ position: 'absolute', top: '0', right: '0', width: '50px', height: '50px' }}
                 />
+                </Link>
               </>
             )}
             {index === 2 && (
+              <Link to= '/clientpage/spacezoompage/B3/C'>
              <img
              src="/C.png"
              alt="C"
              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '10%', height: '10%' }}
             />
+            </Link>
             )}
             {index === 3 && (
-             <img
+              <Link to= '/clientpage/spacezoompage/B3/D'>
+            <img
              src="/D.png"
              alt="D"
              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '10%', height: '10%' }}
             />
+            </Link>
             )}
+
             {index === 4 && (
+              <Link to= '/clientpage/spacezoompage/B3/E'>
              <img
              src="/E.png"
              alt="E"
              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '10%', height: '10%' }}
             />
+            </Link>
             )}
+
             {index === 5 && (
+            <Link to= '/clientpage/spacezoompage/B3/F'>
              <img
              src="/F.png"
              alt="F"
              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '10%', height: '10%' }}
             />
+            </Link>
             )}
           </div>
         ))}
@@ -118,13 +231,13 @@ export default function ClientPage2() {
          {/* Button to switch to ClientPage2 */}
       <Link to="/clientpage">  
         <button className="my-10  bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          切換到ClientPage
+          切換到 B1
         </button>
       </Link>
 
       <Link to="/clientpage/clientpage2">
         <button className="my-10 mx-50 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          切換到ClientPage2
+          切換到 B2
         </button>
       </Link>
 
